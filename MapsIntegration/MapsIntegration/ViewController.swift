577ce1b5bd103a2bfb37ce97cda4c81cc475373b
Luis Alejandro Ramirez <lramirezsuarez@gmail.com>
2017-09-09 13:19:19 -0500

    Added annotations and user annotations, with long press.

--- MapsIntegration/MapsIntegration/ViewController.swift
+++ MapsIntegration/MapsIntegration/ViewController.swift
@@ -29,7 +29,36 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         map.setRegion(region, animated: true)
         
+        let annotation = MKPointAnnotation()
         
+        annotation.title = "Holi lo mejor"
+        
+        annotation.subtitle = "Esta es un annotation"
+        
+        annotation.coordinate = location
+        
+        map.addAnnotation(annotation)
+        
+        let uilongPress = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:))) //The : sends the information to the function
+        
+        uilongPress.minimumPressDuration = 2
+        
+        map.addGestureRecognizer(uilongPress)
+        
+    }
+    
+    func longpress(gestureRecognizer: UIGestureRecognizer) {
+        let touchPoint = gestureRecognizer.location(in: self.map)
+        
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = coordinate
+        annotation.title = "New place"
+        annotation.subtitle = "You did a long press here"
+        
+        map.addAnnotation(annotation)
     }
 
 }
