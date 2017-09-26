170bfd9d9b78014e0e175f3a1c9f50ede809058c
Luis Alejandro Ramirez <lramirezsuarez@gmail.com>
2017-09-26 14:02:36 -0500

    Permisos, muestra de la posicion en el storyboard, y vista.

--- FindingUserLocation/FindingUserLocation/ViewController.swift
+++ FindingUserLocation/FindingUserLocation/ViewController.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    @IBOutlet weak var mapKit: MKMapView!
+    
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation : CLLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let latDelta : CLLocationDegrees = 0.5
+        let lonDelta : CLLocationDegrees = 0.5
+        
+        let span : MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        
+        let location : CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region : MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        self.mapKit.setRegion(region, animated: true)
     }
 
 
